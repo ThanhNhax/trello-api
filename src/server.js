8b45207db47c9bb1bdbd1c7baf2cc82c977e3220
thanhNha <thanhna.k13@gmail.com>
2023-12-26 16:13:10 +0700

    inti code base

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
+
 const app = express()
 
-const host = 'localhost'
-const port = 6060
+const hostname = 'localhost'
+const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, My server Thanh Nhax</h1>')
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port, host, () => {
-  console.log(`Hello, My server Thanh Nhax http://${host}:${port}`)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Thanh Nhax, I am running at ${hostname}:${port}/`)
 })
+
